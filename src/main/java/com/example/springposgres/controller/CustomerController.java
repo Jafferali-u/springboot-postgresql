4999f035e3e44c15c9cc85141818eeb8b461738b
Jafferali-u <jafferali.u1@gmail.com>
2020-04-12 12:21:14 +0530

    Update CustomerController.java

--- src/main/java/com/example/springposgres/controller/CustomerController.java
+++ src/main/java/com/example/springposgres/controller/CustomerController.java
@@ -43,7 +43,7 @@ public class CustomerController {
 	public String test(){
 		
 		
-		return "Welcome to my project version 42\n This is autoscale Testing  ";
+		return "Welcome to my project version 43\n This is autoscale Testing  ";
 	}
 	
 	@PostMapping("/create")
